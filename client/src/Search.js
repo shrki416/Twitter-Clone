1f9b2702a60c4e5e351bda632bf2384d154dc1e2
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-10-21 16:01:15 -0400

    WIP: UI almost finished

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,7 +1,30 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Search() {
-  return <div className="search">I'm the Search component</div>;
+  return (
+    <div className="search">
+      <div className="search__container">
+        <input
+          className="search__container-input"
+          type="text"
+          placeholder="Search Twitter"
+        />
+        <SearchIcon />
+      </div>
+      <div class="tweets-list">
+        <div class="tweets-welcome-message">
+          <h5>Welcome to Twitter!</h5>
+          <p>Use the search above to see what's happening around the world.</p>
+        </div>
+      </div>
+      <div class="next-page-container">
+        <div id="next-page" onclick="onNextPage()">
+          <i class="fas fa-arrow-down"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar">I'm the sidebar component</div>;
+  return (
+    <div className="sidebar">
+      <h5>Trends</h5>
+      <ul>
+        <li>#JavaScript</li>
+        <li>#React</li>
+        <li>#Programming</li>
+        <li>#WebDev</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
