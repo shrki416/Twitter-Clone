90c08e77cbf348f8954d3895b989b5381b0c7b7e
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-10-21 12:21:19 -0400

    🔥 navigation css finished

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Nav.css";
+import { Avatar } from "@material-ui/core";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import profileImg from "./assets/aa.png";
 
 function Nav() {
   return (
     <div className="nav">
-      <div>I'm the Nav</div>
+      <div className="nav__container">
+        <nav>
+          <div className="nav__logo">
+            <TwitterIcon fontSize="large" />
+          </div>
+          <div className="nav__home">
+            <HomeIcon fontSize="large" />
+          </div>
+          <div className="nav__profile">
+            <Avatar alt="photo" src={profileImg} />
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
