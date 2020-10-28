32884a21fe782273e8999ad22668c1ef85e9357e
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-10-28 16:59:27 -0400

    🚀 able to get and display data 🚀

--- client/src/Search.js
+++ client/src/Search.js
@@ -2,45 +2,50 @@ import React, { useState } from "react";
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import axios from "axios";
+import Tweet from "./Tweet";
 
 function Search() {
   const [input, setInput] = useState("");
   const [tweet, setTweet] = useState([]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    axios
+    await axios
       .get(`/tweets?q=${input}`)
       .then((response) => setTweet(response.data))
       .catch((error) => console.error(error));
-
-    console.log(setTweet);
   };
+  console.log(tweet);
+  const tweets = tweet.statuses || [];
+  const tweetCard = tweets.map((tweet) => <Tweet name={tweet.user.name} />);
 
   return (
-    <form className="search" onSubmit={handleSubmit}>
-      <div className="search__input">
-        <input
-          type="text"
-          placeholder="Search Twitter"
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <SearchIcon fontSize="large" />
-      </div>
-
-      <div>
-        <div className="search__welcome-message">
-          <h4>Welcome to Twitter!</h4>
-          <h5>
-            Use the search above to see what's happening around the world.
-          </h5>
+    <>
+      <form className="search" onSubmit={handleSubmit}>
+        <div className="search__input">
+          <input
+            type="text"
+            placeholder="Search Twitter"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <SearchIcon fontSize="large" />
         </div>
-      </div>
-      <div className="search__next-page">
-        <i className="fas fa-arrow-down"></i>
-      </div>
-    </form>
+
+        <div>
+          <div className="search__welcome-message">
+            <h4>Welcome to Twitter!</h4>
+            <h5>
+              Use the search above to see what's happening around the world.
+            </h5>
+          </div>
+        </div>
+        <div className="search__next-page">
+          <i className="fas fa-arrow-down"></i>
+        </div>
+      </form>
+      <div className="tweet">{tweetCard}</div>
+    </>
   );
 }
 
---
+++ client/src/Tweet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tweet({ name }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
+}
+
+export default Tweet;
