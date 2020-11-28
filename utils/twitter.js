15462d61be2cb6f1a452de48091755bcc56f4d43
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-11-28 11:20:39 -0500

    Add end-point that contains fulltext and media

--- utils/twitter.js
+++ utils/twitter.js
@@ -5,7 +5,7 @@ const URL = "https://api.twitter.com/1.1/search/tweets.json";
 
 class Twitter {
   get(q, count) {
-    return axios.get(URL, {
+    return axios.get(`${URL}?&tweet_mode=extended`, {
       params: { q, count },
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_API_BEARER_TOKEN}`,
