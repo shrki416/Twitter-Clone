73366f0b92c57f019ef52125aaa8ea53127e8a41
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-11-30 16:08:09 -0500

    Nav WIP

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,17 +1,41 @@
 import React from "react";
+import { RiHome7Fill } from "react-icons/ri";
+import { BiHash } from "react-icons/bi";
+import { MdNotifications } from "react-icons/md";
+import { BiEnvelope } from "react-icons/bi";
+import { BsBookmark } from "react-icons/bs";
+import { VscListSelection } from "react-icons/vsc";
+import { HiOutlineUser } from "react-icons/hi";
+import { CgMoreO } from "react-icons/cg";
 
 function Nav() {
   return (
     <div className="nav">
-      <h2>Home</h2>
-      <h2>Explore</h2>
-      <h2>Notifications</h2>
-      <h2>Messages</h2>
-      <h2>Bookmarks</h2>
-      <h2>Lists</h2>
-      <h2>Porfile</h2>
-      <h2>More</h2>
-      <button>Tweet</button>
+      <h2>
+        <RiHome7Fill /> Home
+      </h2>
+      <h2>
+        <BiHash /> Explore
+      </h2>
+      <h2>
+        <MdNotifications /> Notifications
+      </h2>
+      <h2>
+        <BiEnvelope /> Messages
+      </h2>
+      <h2>
+        <BsBookmark /> Bookmarks
+      </h2>
+      <h2>
+        <VscListSelection /> Lists
+      </h2>
+      <h2>
+        <HiOutlineUser /> Porfile
+      </h2>
+      <h2>
+        <CgMoreO /> More
+      </h2>
+      <button className="nav-btn">Tweet</button>
     </div>
   );
 }
