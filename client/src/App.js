f7587bf25bece79bb5c03dd9acd66cc97a880459
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-25 19:06:48 -0500

    reset project/redo

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./App.css";
-import Nav from "./Nav";
-import Main from "./Main";
-import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="container">
-      <Nav />
-      <Main />
-      <Sidebar />
+      <h1>Twitter Clone App</h1>
     </div>
   );
 }
